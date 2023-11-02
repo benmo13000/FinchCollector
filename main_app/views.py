5605e7ff3e1370b5975106c8b2b53de5bfe91572
benmo13000 <benmo13000@gmail.com>
2023-11-01 18:21:04 -0700

    finished part 3 (i think)

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+
 # Create your views here.
-
-
-
 def home(request):
     return render(request, 'home.html')
 
@@ -21,3 +20,17 @@ def finches_detail(request, finch_id):
     return render(request, 'finches/detail.html', {
         'finch': finch
     })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finches'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['species', 'color', 'age']
+    success_url = '/finches'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
